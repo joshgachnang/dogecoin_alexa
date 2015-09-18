9cf4d2623b25579722a4905f6c7a8597d6841651
Josh Gachnang <josh@pcsforeducation.com>
2015-09-18 16:09:37 -0700

    Add USD to DOGE and vice versa

--- doge.py
+++ doge.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 
 DOGE_API = ('http://pubapi.cryptsy.com/api.php?method=singlemarketdata'
             '&marketid=182')
-API_VERSION = '1.0'
+
+ALEXA_API_VERSION = '1.0'
+
+intents = []
+launch_intent = None
+end_intent = None
 
 
 @app.route("/")
@@ -16,57 +21,23 @@ def main():
     return 'Hello world! o/'
 
 
-@app.route("/alexa/api", methods=['GET', 'POST'])
+@app.route("/alexa/api", methods=['POST'])
 def apicalls():
+    # global intents
+    # global launch_intent
+
     if request.method == 'POST':
         data = request.get_json()
-        print data
+        print "Received POST: {}".format(data)
         if data['request'].get('type') == 'IntentRequest':
-            return _intent(data)
+            intent_name = data['request'].get('intent', {}).get('name')
+            for intent in intents:
+                if intent.name == intent_name:
+                    return intent.handle(data)
         elif data['request'].get('type') == 'LaunchRequest':
-            return _launch(data)
+            return launch_intent.handle(data)
         elif data['request'].get('type') == 'SessionEndedRequest':
-            return _end(data)
-
-
-@app.route("/doge/price")
-def doge_price():
-    return _get_current_doge_price()
-
-
-def _intent(data):
-    print "Intent", data
-    if data['request'].get('intent', {}).get('name') == 'HelpIntent':
-        return _help(data)
-    elif data['request'].get('intent', {}).get('name') == 'DogeCoin':
-        return _dogecoin(data)
-
-
-def _dogecoin(data):
-    price = float(_get_current_doge_price()) * 1000000
-
-    response = AlexaResponse(
-        'One million Doge coins are worth {0:.2f} dollars. Wow!'.format(price))
-    return response.to_json()
-
-
-def _help(data):
-    return AlexaResponse(
-        'Just ask what the current price is. Much easy. Wow.').to_json()
-
-
-def _launch(data):
-    print "Launch", data
-
-
-def _end(data):
-    print "End", data
-    return json.dumps({'response': 'ok'})
-
-
-def _get_current_doge_price():
-    response = requests.get(DOGE_API)
-    return response.json()['return']['markets']['DOGE']['lasttradeprice']
+            return end_intent.handle(data)
 
 
 def run_app():
@@ -80,13 +51,79 @@ def run_app():
             host='0.0.0.0')
 
 
+def _get_current_doge_price():
+    response = requests.get(DOGE_API)
+    return response.json()['return']['markets']['DOGE']['lasttradeprice']
+
+
+class Intent(object):
+    # The IntentName to respond to
+    name = None
+
+    def handle(self, data):
+        """Handles an IntentRequest.
+
+        :param data: the JSON data sent by Amazon
+        """
+        raise NotImplementedError()
+
+
+class NoopIntent(object):
+    def handle(self, data):
+        print 'Noop'
+        return json.dumps({})
+
+
+class DogeCoinIntent(Intent):
+    name = 'DogeCoin'
+
+    def handle(self, data):
+        price = float(_get_current_doge_price()) * 1000000
+
+        response = AlexaResponse('One million Doge coins are worth {0:.2f} '
+                                 'dollars. Wow!'.format(price))
+        return response.to_json()
+
+
+class USDtoDoge(Intent):
+    name = 'USDtoDOGE'
+
+    def handle(self, data):
+        price = float(_get_current_doge_price())
+        dollars = int(data['request']['intent']['slots']['dollars']['value'])
+        return AlexaResponse('{0} dollars is worth {1:.2f} dogecoins'.format(
+            dollars, dollars / price)).to_json()
+
+
+class DogeToUSD(Intent):
+    name = 'DOGEtoUSD'
+
+    def handle(self, data):
+        price = float(_get_current_doge_price())
+        coins = int(data['request']['intent']['slots']['coins']['value'])
+        return AlexaResponse(
+            '{0} dogecoins is worth {1:} dollars and {2} cents'.format(
+                coins,
+                int(coins * price),
+                int((coins * price - int(coins * price)) * 100))).to_json()
+
+
+class HelpIntent(Intent):
+    name = 'HelpIntent'
+
+    def handle(self, data):
+        return AlexaResponse(
+            'Just ask what the current price is. Much easy. Wow.').to_json()
+
+
 class AlexaResponse(object):
     def __init__(self, speech):
         self.speech = speech
 
     def to_json(self):
+        # TODO(pcsforeducation) add card support
         response = {
-            "version": API_VERSION,
+            "version": ALEXA_API_VERSION,
             "response": {
                 "outputSpeech": {
                     "type": "PlainText",
@@ -99,4 +136,22 @@ class AlexaResponse(object):
 
 
 if __name__ == "__main__":
+    # Each of these intent classes name attrs will be matched against intent
+    # name sent by Amazon
+    ENABLED_INTENTS = [
+        DogeCoinIntent,
+        DogeToUSD,
+        USDtoDoge,
+        HelpIntent,
+    ]
+
+    # The intent to handle launch events like 'Open Dogecoin'
+    launch_intent = DogeCoinIntent()
+
+    # The intent to handle event at the end of a session
+    end_intent = NoopIntent
+
+    for intent in ENABLED_INTENTS:
+        intents.append(intent())
+
     run_app()
